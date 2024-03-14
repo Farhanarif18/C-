e61b3f3bc92d807eb39800b2d4fe97efdda454f7
Farhan Arif <162949751+Farhanarif18@users.noreply.github.com>
2024-03-14 18:01:43 +0800

    For

---
+++ belajar for.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	for(int i = 1; i<=5; i++){
+		for(int a = 1; a<=i; a++){
+		
+		cout<<"*";
+	}
+	
+	 cout<<endl;
+	}
+	
+	for(int i = 1; i<=5; i++){
+		for(int a = 5; a>=i; a--){
+		
+		cout<<"*";
+	}
+	
+	 cout<<endl;
+	}
+}
